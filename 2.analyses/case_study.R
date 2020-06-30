13ede10a8a809229a087b93c8d394f5bd3fc37b9
Malyon - Uni <malyonbimler@gmail.com>
2020-06-30 13:32:31 +1000

    init code for case study results to go in mss

---
+++ 2.analyses/case_study.R
@@ -0,0 +1,11 @@
+
+
+# Case study for Methods - on Trace's 'full' community dataset
+
+setwd('~/Dropbox/Work/Projects/2020_Methods_for_compnet/')
+
+load('3.case_study/model/transformed/scaled_alpha_matrices.Rdata')
+
+# Do stuff on the alphas
+
+
