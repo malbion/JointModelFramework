ad717efc395e436b3a2742752099227a9aeee156
Malyon - Uni <malyonbimler@gmail.com>
2020-11-11 18:06:09 +1000

    init files that will be included as code for the mss

---
+++ 5.code_for_openss/data_prep.R
@@ -0,0 +1,81 @@
+# This function takes a dataframe and returns a list (stan.data)
+# with all the information necessary for STAN to run the model
+
+# The dataframe should be organised with 1 observation of individual performance per row 
+# Columns should include the measure of performance (perform), a column identifying which 
+# species or component/group that observation belongs to (focal), and a column for each
+# potential neighbouring species or component counting their abundance for that observation
+# e.g.: 
+
+#   focal  |  seeds  | species1 | species2 | species3
+# ---------------------------------------------------
+# species1 |   5     |    0     |    2     |    1
+# species1 |   2     |    3     |    1     |    0
+# species2 |   10    |    1     |    1     |    4
+
+  
+  
+data_prep <- function(perform = "seeds", # column name for performance indicator
+                      focal = 'focal',   # column name for species or component indicator
+                      nonNcols = 2,      # number of columns before neighbour abundances begin
+                      df = NULL,       # dataframe object
+                      ...){
+  
+
+  # set up the data in list format as preferred by STAN:
+  stan.data <- list()
+  
+  # create a matrix which tallies the number of observations for each focal and neighbour
+  counts <- df[ , -c(1:nonNcols)] # keep only neighbour abundances
+  counts[counts>0] <- 1
+  counts <- split(counts, as.factor(df[ , focal]))
+  obs <- do.call(rbind, lapply(counts, colSums))
+  
+  # integers
+  stan.data$S <- length(unique(df[ , focal]))  # number of focal components (species)
+  stan.data$N <- nrow(df)                      # number of observations
+  stan.data$K <- ncol(df[ , -c(1:nonNcols)])   # number of neighbours
+  stan.data$I <- length(obs[obs>0])              # number of observed interactions
+
+  
+  # vectors
+  stan.data$species_ID <- as.numeric(as.factor(df[ , focal]))
+  stan.data$performance <- df[ , perform]
+  
+  
+  # set up indices to place observed interactions in the alpha matrix
+  # first count the number of interactions observed for each focal species
+  stan.data$inter_per_species <- obs
+  stan.data$inter_per_species[stan.data$inter_per_species > 0] <- 1
+  stan.data$inter_per_species <- rowSums(stan.data$inter_per_species)
+  # column index in the alpha matrix for each observed interaction
+  stan.data$icol <- unlist(apply(ifelse(obs > 0, T, F), 1, which))
+  names(stan.data$icol) <- NULL
+  stan.data$icol <- as.vector(stan.data$icol)
+  # begin the row index
+  stan.data$irow <- rep(1, stan.data$inter_per_species[[1]])
+  # begin the start and end indices for the vector of interactions per species
+  stan.data$istart <- 1
+  stan.data$iend <- stan.data$inter_per_species[[1]] #???
+  
+  # populate indices for all the other species
+  for(s in 2:stan.data$S) {
+    # starting position of a_ij's for i in the vector of observed interactions (ie the 1st 'j')
+    stan.data$istart[s] <- sum(stan.data$inter_per_species[s-1:s])+1
+    # end position of a_ij's for i in the vector of observed interactions (the last 'j')
+    stan.data$iend[s] <-  sum(stan.data$inter_per_species[1:s])
+    # row index in the alpha matrix for each observed interaction
+    stan.data$irow <- c(stan.data$irow, rep(s, stan.data$inter_per_species[[s]]))
+  }
+  
+  # model matrix
+  stan.data$X <- as.matrix(df[ , -c(1:nonNcols)]) 
+  
+  # Number of observations per interaction - for model weighting?
+  stan.data$Obs <- as.vector(apply(obs, 1, c))   # vector of the number of observations for each interactions
+  stan.data$Obs <- stan.data$Obs[stan.data$Obs>0] # remove unobserved interactions
+  
+  # Done!
+  return(stan.data)
+  
+}
---
+++ 5.code_for_openss/master.R
@@ -0,0 +1,84 @@
+# Running the joint model on simulated data 
+
+
+# set up R environment
+require(rstan)
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores()) 
+
+library(rethinking)
+library(reshape2)
+
+# load required functions
+source('data_prep.R')
+source('return_inter_array.R')
+
+# load the data 
+df <- read.csv('simdata.csv', stringsAsFactors = F)
+
+# prepare the data into the format required by STAN and the model code
+stan.data <- data_prep(perform = 'seeds', 
+                       focal = 'focal', 
+                       nonNcols = 4, 
+                       df = df)
+
+# identify focal and neighbouring species to be matched to parameter estimates
+focalID <- unique(df$focal)  # this should return the names of unique focal groups in the order
+# in which they are encountered in the dataframe
+neighbourID <- colnames(df[ , -c(1:4)])
+
+message(paste0('Data dimensions = ', dim(df)[1], ', ', dim(df)[2]))
+message(paste0('Number of focal groups = ', length(focalID)))
+message(paste0('Number of neighbour groups = ', length(neighbourID)))
+
+
+# Run the model! 
+fit <- stan(file = 'joint_model.stan',
+            data =  stan.data,               # named list of data
+            chains = 4,
+            warmup = 1000,          # number of warmup iterations per chain
+            iter = 5000,            # total number of iterations per chain
+            refresh = 100,         # show progress every 'refresh' iterations
+            control = list(max_treedepth = 10)
+)
+
+# Get the full posteriors 
+joint.post.draws <- extract.samples(fit)
+
+# Select parameters of interest
+param.vec <- c('a', 'beta_ij', 'effect', 'response', 're', 'inter_mat',
+               'mu', 'disp_dev', 'sigma_alph')
+
+# Draw 1000 samples from the 80% posterior interval for each parameter of interest
+p.samples <- list()
+p.samples <- sapply(param.vec[param.vec != 'sigma_alph' & param.vec != 'inter_mat'], function(p) {
+  p.samples[[p]] <- apply(joint.post.draws[[p]], 2, function(x){
+    sample(x[x > quantile(x, 0.1) & x < quantile(x, 0.9)], size = 1000)
+  })  # this only works for parameters which are vectors
+})
+# there is only one sigma_alph parameter so we must sample differently:
+p.samples[['sigma_alph']] <- sample(joint.post.draws$sigma_alph[
+  joint.post.draws$sigma_alph > quantile(joint.post.draws$sigma_alph, 0.1) & 
+    joint.post.draws$sigma_alph < quantile(joint.post.draws$sigma_alph, 0.9)], size = 1000)
+
+# WARNING: in the STAN model for annual wildflowers, parameter 'a' lies within an exponential,
+# 'a' estimates must thus be exponentiated to return estimates of intrinsic performance
+intrinsic.perf <- exp(p.samples$a)
+colnames(intrinsic.perf) <- focalID
+
+# Build matrices of interaction estimates
+#----------------------------------------
+inter_mat <- return_inter_array(joint.post.draws, 
+                                response = p.samples$response,
+                                effect = p.samples$effect,
+                                focalID,
+                                neighbourID)
+# inter_mat is now a 3 dimensional array, where rows = focals, columns = neighbours and 3rd dim = samples from the posterior
+# inter_mat[ , , 1] should return a matrix consisting of one sample for every interaction 
+# apply(inter_mat, c(1, 2), mean) will return the mean estimate for every interaction (NB: this is the 
+# mean of the 80% posterior interval, so will be slightly different to the mean value returned from 
+# summary(fit), which is calculated from the full posterior distribution) 
+
+# Interactions can now be divided by the appropriate scaling (intrinsic performance, and demographic rates
+# if a population dynamics model is used) in order to return per capita interaction strengths. 
+
