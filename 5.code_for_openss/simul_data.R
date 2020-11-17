bc210a865bba7d70fbabacd3fd77d402a61c3168
Malyon - Uni <malyonbimler@gmail.com>
2020-11-17 12:35:13 +1000

    init function to create simulated dataset

---
+++ 5.code_for_openss/simul_data.R
@@ -0,0 +1,9 @@
+# Function to create a simulated dataset 
+
+
+S <- 20   # number of focal groups / species
+K <- 20   # number of neighbour focals 
+pI <- 0.75   # proportion of interactions which are observed 
+
+
+
