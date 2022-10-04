18f53c08a223811c9e936881e8795a26fb4367b8
Malyon - Uni <malyonbimler@gmail.com>
2022-10-04 11:34:20 +1000

    adding g_j to the scaling of interactions

--- 2.case_study/functions/scale_interactions.R
+++ 2.case_study/functions/scale_interactions.R
@@ -1,6 +1,9 @@
 # Scale the interaction parameters into betas
 # Returns a 3D array of dimensions [N_species, N_neighbs, N_samples]
 
+# NB: demographic scaling can only be done on those interactions where we have germination
+# rates for both i and j aka interactions between focals only.
+
 scale_interactions <- function(betas,
                                lambdas,   # growth.rates.samples
                                key_speciesID,
@@ -15,7 +18,8 @@ scale_interactions <- function(betas,
   # average seed rates 
   seeds <- read.csv('data/seed_rates.csv', row.names = 1)
   # Scale lambdas into growth rates including the seed rates (demographic param)
-  # r_i = ln(eta) = ln( (g_i * lambda_i ) / (1 - (1 - g_i) s_i) )
+  # r_i = ln(eta) = ln( (g_i * lambda_i ) / (1 - (1 - g_i) s_i) ) --- see Eq 15 Supps
+  # reminder: lambda_i = exp(gamma_i) = exp(beta_i0) in old code
     r_i <- sapply(c(1:length(key_speciesID)), function(x) {
     prod <- seeds[key_speciesID[x], 'germ'] / (1 - (1 - seeds[key_speciesID[x], 'germ'])*seeds[key_speciesID[x], 'surv'])
     if (prod < 0.1) {prod <- 0.1}
@@ -27,7 +31,7 @@ scale_interactions <- function(betas,
     message(paste0('Growth rates for community ', comm))
     print(ri_means)
     
-    #----------------- Removing growth rates < 1 ?? ------------------------------------------#
+    #----------------- Removing growth rates < 1 ------------------------------------------#
     if(length(ri_means[ri_means < 1]) > 0){
       # If any growth rates are below 1 .... 
       to.rplc <- names(ri_means[ri_means < 1])
@@ -41,48 +45,46 @@ scale_interactions <- function(betas,
         r_i[ , i] <- r_i[, rplc.sp]
       }
  }
-  
     #-------------------------------------------------------------------------------------#
     write.csv(r_i, paste0('model/transformed/ri_demogrwth.csv'), row.names = T)
     
-    
     # Scale the betas by dividing by growth rate
-    # scaled beta = model_param/r_i = model_param/ln(eta)
-    # reminder: model_param == alpha*g (in plant pop dyn model)
-    beta_mat <- sapply(c(1:length(key_speciesID)), function(x) {
-      # sample each interaction from the community matrix
-      bij_samples <- t(betamat[x, , ])
-      # sample the growth rates
-      ri_samples <- r_i[ , x]
-      # divide each row - aij_samples must be [N_samples, N_neighbours]
-      bij_samples/ri_samples
+    # scaled beta = (g_j * model_param_ij) / r_i 
+    # 1. g_j * model_param_ij: 
+    beta.gj <- sapply(c(1:length(key_speciesID)), function(j) {
+      # dimensions: betamat[ i, j, samples]
+      seeds[j, 'germ'] * betamat[ , j, ]
     }, simplify = 'array', USE.NAMES = T)
+    # dimensions: beta.gj[i, samples, j]
+    # 2. beta.gj / r_i
+    bij.scaled <- sapply(c(1:length(key_speciesID)), function(i) {
+      beta.gj[i, , ] / r_i[ , i]
+    }, simplify = 'array', USE.NAMES = T)
+    # dimensions: bij.scaled[samples, j, i]
     
-    # SAMPLE from the 80% CI for each tranformed beta
-    beta_df <- as.data.frame(beta_mat)
-    beta_CI <- apply(beta_df, 2, function(param) {
-      sample(param[param > quantile(param, 0.1) & 
-                     param < quantile(param, 0.9)], size = 1000)
+    # SAMPLE from the 80% CI for each scaled beta
+    beta_CI <- apply(bij.scaled, c(2,3), function(p) {
+      sample(p[p > quantile(p, 0.1) & 
+                     p < quantile(p, 0.9)], size = 1000)
     })
-    # get betas back into a focals / neighbours / samples format
-    beta_scaled <- array(data = unlist(beta_CI), 
-                          dim = c(1000, length(key_neighbourID), length(key_speciesID)), 
-                          dimnames = list('samples' = seq(1, 1000), 'neighbour' = key_neighbourID, 'species' = key_speciesID))
-    beta_scaled <- aperm(beta_scaled, c(3,2,1))
+    # dimensions: beta_CI[samples, j, i]
     
-    dimnames(beta_scaled)[[1]] <- key_speciesID
-    names(dimnames(beta_scaled))[1] <- 'species'
-    dimnames(beta_scaled)[[3]] <- seq(1:1000)
-    names(dimnames(beta_scaled))[3] <- 'samples'
-    attributes(dimnames(beta_scaled)[[1]]) <- NULL    
-    attributes(dimnames(beta_scaled)[[2]]) <- NULL
+    # reorder into [i, j, samples]
+    final.betas <- aperm(beta_CI, c(3, 2, 1))
+    dimnames(final.betas) <- list('species' = key_speciesID,
+                                  'neighbour' = key_speciesID,
+                                  'samples' = seq(1, 1000))
+    
+    attributes(dimnames(final.betas)[[1]]) <- NULL    
+    attributes(dimnames(final.betas)[[2]]) <- NULL
 
   # Check that the rows and columns are properly ordered
-  if(identical(dimnames(beta_scaled)[[1]], dimnames(beta_scaled)[[2]][1:dim(beta_scaled)[[1]]]) == F) {
+  if(identical(dimnames(final.betas)[[1]], 
+               dimnames(final.betas)[[2]][1:dim(final.betas)[[1]]]) == F) {
     message('WARNING: rownames and column names do no match')
   }
   
-  return(beta_scaled)
+  return(final.betas)
   
   }
 
